4cd6bab57ba3a5044070e4601c72b89181e635cf
Paola Garcia <paogaram3@gmail.com>
2026-04-30 19:49:47 -0700

    Updated files
    Nav Bar CSS and JS

---
+++ script.js
@@ -0,0 +1,5 @@
+const hamburger = document.querySelector('#hamburger');
+const navMenu = document.querySelector('#nav-menu');
+hamburger.addEventListener('click', function() {
+  navMenu.classList.toggle('active');
+});
